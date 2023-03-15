6b7f3024655b345222e07a8485db772d5b19a959
jmloeches <jmloeches@igae.hacienda.gob.es>
2023-03-15 18:47:31 +0100

    Hacer un commit en rama 1

--- src/main/java/local/loeches/restfull/CCliente.java
+++ src/main/java/local/loeches/restfull/CCliente.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "cliente")
+@Table(name = "cliente") //prueba de git
 public class CCliente {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
